3f9481097d9c8dadfc1c27a65c4d316b5b7c6304
rensoostenbach <rensoostenbach@hotmail.com>
2021-06-16 17:46:31 +0200

    Categorical distribution
    Still need to fix the calculation of log likelihood from probs and x

---
+++ main_categorical.py
@@ -0,0 +1,111 @@
+from torch import optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, Subset
+from modules import *
+from modules_beta import *
+from modules_categorical import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def load_data():
+    mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
+    train_set = Subset(mnist_trainset, range(40000))
+    validation_set = Subset(mnist_trainset, range(40000, 50000))
+    test_set = Subset(mnist_trainset, range(50000, 60000))
+    return train_set, validation_set, test_set
+
+def train(model, device, train_dataloader, validation_dataloader, loss_function, optimizer, num_lags=4):
+    training = True
+    lag_valloss = [np.inf for _ in range(num_lags)]
+    epoch = 1
+    while training:
+        train_epoch_elbo, train_epoch_logp, train_epoch_kl = [], [], []
+        val_epoch_elbo, val_epoch_logp, val_epoch_kl = [], [], []
+        model.train()
+        for batch in train_dataloader:
+            optimizer.zero_grad()
+            x, _ = batch
+            x = x.to(device)
+            z, mu_enc, log_sig_enc = model.encoder(x)
+            x_reconstr, probs = model.decoder(z)
+
+            boundaries = torch.tensor([0, 0.2, 0.4, 0.6, 0.8, 1]).to(device)
+            # Need to do -1 here and relu activation to make sure the bucketization works in the intended way, and that
+            # the -1 goes back to a 0.
+            # The intended way is: 0-0.2: 0, 0.2-0.4: 1, 0.4-0.6: 2, 0.6-0.8: 3, 0.8-1: 1
+            x_bucketized = torch.nn.functional.relu(torch.bucketize(x, boundaries) - 1, inplace=True)
+
+            elbo, logp, kl = loss_function(x_bucketized, probs, mu_enc, log_sig_enc)
+            elbo.backward()
+            optimizer.step()
+            train_epoch_elbo.append(elbo.cpu().item() / x.size()[0])
+            train_epoch_logp.append(logp.cpu().item() / x.size()[0])
+            train_epoch_kl.append(kl.cpu().item() / x.size()[0])
+        model.eval()
+        with torch.no_grad():
+            for batch in validation_dataloader:
+                x, _ = batch
+                x = x.to(device)
+                z, mu_enc, log_sig_enc = model.encoder(x)
+                x_reconstr, probs = model.decoder(z)
+
+                boundaries = torch.tensor([0, 0.2, 0.4, 0.6, 0.8, 1]).to(device)
+                x_bucketized = torch.bucketize(x, boundaries).to(device)
+
+                elbo, logp, kl = loss_function(x_bucketized, probs, mu_enc, log_sig_enc)
+                val_epoch_elbo.append(elbo.cpu().item() / x.size()[0])
+                val_epoch_logp.append(logp.cpu().item() / x.size()[0])
+                val_epoch_kl.append(kl.cpu().item() / x.size()[0])
+            if epoch % 5 == 0:
+                plt.figure()
+
+                # subplot(r,c) provide the no. of rows and columns
+                f, axarr = plt.subplots(7, 3)
+
+                # use the created array to output your multiple images. In this case I have stacked 4 images vertically
+                for i in range(7):
+                    axarr[i, 0].imshow(x[i].cpu().permute(1, 2, 0).numpy())
+                    # axarr[i, 1].imshow(mu_dec[i].cpu().permute(1, 2, 0).numpy())
+                    axarr[i, 2].imshow(x_reconstr[i].cpu().permute(1, 2, 0).numpy())
+                plt.show()
+
+        print("Epoch loss in epoch {}: {}, logP(X): {}, KL Divergence: {}".format(epoch, np.mean(np.array(train_epoch_elbo)), np.mean(np.array(train_epoch_logp)), np.mean(np.array(train_epoch_kl))))
+        print("Epoch validation loss in epoch {}: {}, logP(X): {}, KL Divergence: {}".format(epoch, np.mean(np.array(val_epoch_elbo)), np.mean(np.array(val_epoch_logp)), np.mean(np.array(val_epoch_kl))))
+        lag_valloss.append(np.mean(np.array(val_epoch_elbo)))
+        if sorted(lag_valloss) == lag_valloss:
+            training = False
+        else:
+            lag_valloss = lag_valloss[1:]
+            epoch += 1
+    return model
+
+if __name__ == '__main__':
+    batch_size = 1000
+    train_set, validation_set, test_set = load_data()
+    train_set = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    validation_set = DataLoader(validation_set, batch_size=batch_size, shuffle=True)
+    test_set = DataLoader(test_set, batch_size=batch_size, shuffle=True)
+    num_var_enc = 1
+    num_var_dec = 5  # Since we choose 5 bins, we need the decoder to output a softmax of 5 probabilities
+    # TODO: Run 2 and 16
+    num_latent = 2
+    num_neurons = [8, 16, 24, 32]
+    dropout = 0.2
+    maxpool = [0, 1, 2]
+    enc = CategoricalEncoder(num_var_enc, num_latent, num_neurons, dropout, maxpool)
+    dec = CategoricalDecoder(num_var_dec, num_latent, num_neurons, dropout, maxpool)
+    model = CategoricalVAE(enc, dec).to(device)
+    loss_function = CategoricalELBOLoss()
+    optimizer = optim.Adam(model.parameters())
+    model = train(model, device, train_set, test_set, CategoricalELBOLoss(), optimizer, num_lags=3)
+    torch.save(model.state_dict(), "Categorical_beta_2_dimensional")
+    model.eval()
+    with torch.no_grad():
+        sample = model.sample(10)
+        for i in range(10):
+            plt.figure()
+            plt.imshow(sample[i].cpu().permute(1, 2, 0).numpy())
+            plt.show()
---
+++ modules_categorical.py
@@ -0,0 +1,133 @@
+import torch
+import torch.nn.functional as F
+from torch.distributions import Categorical
+from torch import nn
+import numpy as np
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class CategoricalEncoder(nn.Module):
+    def __init__(self, num_var, num_latent, num_neurons, dropout, maxpool_indices, batch_norm=True, flatten=False):
+        super(CategoricalEncoder, self).__init__()
+        self.num_var = num_var
+        self.num_latent = num_latent
+        self.num_neurons = num_neurons
+        self.maxpool_indices = [(3 + batch_norm) * x for x in maxpool_indices]
+        self.dims = [28, 14, 7, 3, 1]
+
+        num_units = [num_var] + num_neurons
+        layers = []
+        for n_prev, n_new in zip(num_units[0:-1], num_units[1:]):
+            layers.append(nn.Conv2d(n_prev, n_new, 3, padding=1))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout))
+            if batch_norm:
+                layers.append(nn.BatchNorm2d(n_new))
+        self.flatten = flatten
+
+
+        self.mu = nn.Linear((self.dims[len(maxpool_indices)] ** 2) * num_neurons[-1], num_latent)
+        self.log_var = nn.Linear((self.dims[len(maxpool_indices)] ** 2) * num_neurons[-1], num_latent)
+        self.layers = nn.ModuleList(layers)
+        self.var_act = nn.Softplus()
+
+
+    def forward(self, x):
+        h = x
+        for i, layer in enumerate(self.layers):
+            if i in self.maxpool_indices:
+                h = F.max_pool2d(layer(h), 2)
+            else:
+                h = layer(h)
+        h = h.view(h.shape[0], -1)
+        mu = self.mu(h)
+        log_sigma = self.var_act(self.log_var(h))
+        z = self.reparameterization(mu, log_sigma)
+
+        return z, mu, log_sigma
+
+    def reparameterization(self, mu, log_sigma):
+        sigma = torch.exp(log_sigma)
+        epsilon = torch.rand_like(sigma).to(device)
+        z = mu + sigma * epsilon
+
+        return z
+
+class CategoricalDecoder(nn.Module):
+    def __init__(self, num_var, num_latent, num_neurons, dropout, upsample_indices, batch_norm=True):
+        super(CategoricalDecoder, self).__init__()
+        self.num_var = num_var
+        self.num_latent = num_latent
+        self.num_neurons = num_neurons
+        self.upsample_indices = [(3 + batch_norm) * (len(num_neurons) - x) - 2 for x in upsample_indices]
+        self.num_neurons.reverse()
+        self.dims = [28, 14, 7, 3, 1]
+
+        num_units = [num_latent] + self.num_neurons
+
+        layers = [nn.Linear(num_latent, (self.dims[len(self.upsample_indices)] ** 2) * num_latent)]
+
+        for n_prev, n_new in zip(num_units[0:-1], num_units[1:]):
+            layers.append(nn.ConvTranspose2d(n_prev, n_new, kernel_size=3, padding=1))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout))
+            if batch_norm:
+                layers.append(nn.BatchNorm2d(n_new))
+        self.layers = nn.ModuleList(layers)
+        self.probs = nn.Conv2d(num_neurons[-1], num_var, kernel_size=3, padding=1)
+        self.probs_act = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        h = x
+
+        h = self.layers[0](h)
+        h = h.view(h.shape[0], self.num_latent, self.dims[len(self.upsample_indices)],
+                   self.dims[len(self.upsample_indices)])
+        for i, layer in enumerate(self.layers[1:]):
+            if i in self.upsample_indices:
+                index = self.upsample_indices.index(i)
+                h = F.interpolate(layer(h), self.dims[index])
+            else:
+                h = layer(h)
+
+        h = self.probs(h)
+        probs = self.probs_act(h).permute(0,3,2,1)
+
+        m = Categorical(probs)
+        z = m.sample()
+
+        # Translate bin-assignments to numerical values using midpoint
+        z = (z/5)+0.1
+
+        return z, probs
+
+
+class CategoricalVAE(nn.Module):
+    def __init__(self, encoder, decoder):
+        super(CategoricalVAE, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, x):
+        z, mu_enc, log_sigma_enc = self.encoder(x)
+        x_reconstr, probs = self.decoder(z)
+        return x_reconstr
+
+    def sample(self, n):
+        noise = torch.rand((n, self.encoder.num_latent)).to(device)
+        return self.decoder(noise)[0]
+
+
+class CategoricalELBOLoss(nn.Module):
+    def __init__(self):
+        super(CategoricalELBOLoss, self).__init__()
+        self.epsilon = 1e-16
+
+    def forward(self, x, probs, enc_mu, enc_var):
+        likelihoods = []
+        probs = probs.reshape(-1, 5)
+        x = x.reshape(-1)
+
+        log_p = torch.sum()
+        KL = -0.5 * torch.sum(1 + enc_var - (enc_mu ** 2) - torch.exp(enc_var))
+        return -log_p + KL, log_p, KL
