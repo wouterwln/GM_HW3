f768024b616a87476163d08e4b53073c6d6bbada
rensoostenbach <rensoostenbach@hotmail.com>
2021-06-17 14:10:27 +0200

    Include code for ELBO graph and 32 samples

--- task_2/main_beta.py
+++ task_2/main_beta.py
@@ -18,6 +18,8 @@ def load_data():
 def train(model, device, train_dataloader, validation_dataloader, loss_function, optimizer, num_lags=4):
     training = True
     lag_valloss = [np.inf for _ in range(num_lags)]
+    train_loss = []
+    val_loss = []
     epoch = 1
     while training:
         train_epoch_elbo, train_epoch_logp, train_epoch_kl = [], [], []
@@ -62,11 +64,20 @@ def train(model, device, train_dataloader, validation_dataloader, loss_function,
         print("Epoch loss in epoch {}: {}, logP(X): {}, KL Divergence: {}".format(epoch, np.mean(np.array(train_epoch_elbo)), np.mean(np.array(train_epoch_logp)), np.mean(np.array(train_epoch_kl))))
         print("Epoch validation loss in epoch {}: {}, logP(X): {}, KL Divergence: {}".format(epoch, np.mean(np.array(val_epoch_elbo)), np.mean(np.array(val_epoch_logp)), np.mean(np.array(val_epoch_kl))))
         lag_valloss.append(np.mean(np.array(val_epoch_elbo)))
+        train_loss.append(-np.mean(np.array(train_epoch_elbo)))
+        val_loss.append(-np.mean(np.array(val_epoch_elbo)))
         if sorted(lag_valloss) == lag_valloss:
             training = False
         else:
             lag_valloss = lag_valloss[1:]
             epoch += 1
+    plt.figure()
+    plt.plot(train_loss, label="Train")
+    plt.plot(val_loss, label="Validation")
+    plt.xlabel("Epoch")
+    plt.ylabel("ELBO")
+    plt.legend()
+    plt.show()
     return model
 
 if __name__ == '__main__':
@@ -77,7 +88,7 @@ if __name__ == '__main__':
     test_set = DataLoader(test_set, batch_size=batch_size, shuffle=True)
     num_var = 1
     # TODO: Run 2 and 16
-    num_latent = 2
+    num_latent = 16
     num_neurons = [8, 16, 24, 32]
     dropout = 0.2
     maxpool = [0, 1, 2]
@@ -86,11 +97,12 @@ if __name__ == '__main__':
     model = BetaVAE(enc, dec).to(device)
     optimizer = optim.Adam(model.parameters())
     model = train(model, device, train_set, test_set, BetaELBOLoss(), optimizer, num_lags=3)
-    torch.save(model.state_dict(), "VAE_beta_2_dimensional")
+    torch.save(model.state_dict(), "VAE_beta_16_dimensional")
     model.eval()
     with torch.no_grad():
-        sample = model.sample(10)
-        for i in range(10):
-            plt.figure()
-            plt.imshow(sample[i].cpu().permute(1, 2, 0).numpy())
-            plt.show()
+        sample = model.sample(16)
+        f, axarr = plt.subplots(4, 4)
+        for i in range(16):
+
+            axarr[i // 4, i % 4].imshow(sample[i].cpu().permute(1, 2, 0).numpy())
+        plt.show()
